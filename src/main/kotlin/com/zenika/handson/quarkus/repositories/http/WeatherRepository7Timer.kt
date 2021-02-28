75f28b398f5c7b2b91e0f26f134f41f0dc8638c0
Martin Choraine <martin.choraine@gmail.com>
2021-02-28 20:20:46 +0100

    step 6: add cache for remote service calls

--- src/main/kotlin/com/zenika/handson/quarkus/repositories/http/WeatherRepository7Timer.kt
+++ src/main/kotlin/com/zenika/handson/quarkus/repositories/http/WeatherRepository7Timer.kt
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.zenika.handson.quarkus.entities.City
 import com.zenika.handson.quarkus.entities.Weather
 import com.zenika.handson.quarkus.repositories.WeatherRepository
+import io.quarkus.cache.CacheResult
 import org.eclipse.microprofile.rest.client.inject.RestClient
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class WeatherRepository7Timer(@RestClient val restClient7Timer: RestClient7Timer, val objectMapper: ObjectMapper) : WeatherRepository {
 
+    @CacheResult(cacheName = "weather")
     override fun getDailyWeather(city: City): List<Weather> {
         return restClient7Timer.getWeather(city.position.longitude, city.position.latitude)
             .let { objectMapper.readValue(it, DailyWeather7Timer::class.java) }
